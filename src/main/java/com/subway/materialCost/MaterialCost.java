4e71a3c87cf5269d0e42c3a6be182ef474a2b6b7
huangbin <876301469@qq.com>
2018-03-14 22:06:22 +0800

    c

---
+++ src/main/java/com/subway/materialCost/MaterialCost.java
@@ -0,0 +1,44 @@
+package com.subway.materialCost;
+
+
+import com.subway.location.Location;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by huangbin
+ * 2016年10月14日09:26:04
+ * 工单物料消耗信息
+ */
+@Entity
+@Table(name = "T_MATERIAL_COST")
+@Data
+public class MaterialCost implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 100)
+    private Location location; //位置描述
+
+    @Column(length = 50)
+    private String ecName; //易耗品名称
+
+    @Column(length = 3)
+    private Integer amount; //位置描述
+
+    @Column(length = 20)
+    private String applyDate; //位置描述
+
+    @Column(length = 1)
+    private String status; //状态
+
+    @Column(length = 20)
+    private String authKey; //授权码
+
+}
